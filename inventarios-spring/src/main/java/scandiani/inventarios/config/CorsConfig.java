0fbcfedadfd3f1def90beabdfd7cd9685c8cc6a3
Fernando10005 <149250751+Fernando10005@users.noreply.github.com>
2026-04-17 00:37:15 -0600

    Create CorsConfig.java

---
+++ inventarios-spring/src/main/java/scandiani/inventarios/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package scandiani.inventarios.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/inventario-app/**")
+                .allowedOrigins(
+                    "https://intelligent-empathy-production.up.railway.app",
+                    "http://localhost:4200"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
